c0f110e973f354abb835631e2ddf0d7b886d6738
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-15 02:15:29 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdat < ActiveRecord::Migration[5.1]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change 
     change_column :students, :birthdate, :datetime
   end
